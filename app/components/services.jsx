cfd8b3c1ffd94b62fd6a70012b00639ab2ce72c5
Zeta <ag.sebastian912@gmail.com>
2024-09-07 07:22:18 -0300

    Services Section

--- app/components/services.jsx
+++ app/components/services.jsx
@@ -1,41 +1,51 @@
 import React from 'react'
+import Image from 'next/image'
+import Test from '../../public/test.jpg'
 
 const Services = () => {
   return (
-    <div className='bg-white flex justify-center -mt-px top-0 text-black'>
-        <div className='w-full justify-center mt-[150px]'>
-            <h2 className='text-black md:text-4xl lg:text-5xl font-semibold text-center'>Servicios</h2>
+    <div className='bg-white h-[95vh] justify-center -mt-px top-0 text-black'>
+    <div className='w-full justify-center mt-[150px]'>
+        <h2 className='text-black md:text-4xl lg:text-5xl font-semibold text-center'>Servicios</h2>
+    </div>
+    <div className='md:max-w-[1440px] flex flex-col lg:flex-row justify-center items-center mt-[100px] lg:space-x-8 mx-auto'>
+        {/* Card 1 */}
+        <div className='max-w-[300px] md:max-w-[400px] border-2 flex-col border-orange-500 rounded-2xl min-h-[500px] max-h-[500px] mt-[50px] lg:mt-0'>
+            <Image className='max-h-[250px] rounded-t-[16px] object-cover' src={Test} alt="" />
+            <h3 className='text-left px-4 text-orange-500 text-2xl font-bold mt-[20px]'>Marketing Digital</h3>
+            <p className='text-left text-base font-light px-4'>
+                Norem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Etiam eu turpis molestie, dictum est a, mattis tellus. 
+                Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, 
+                ut interdum tellus elit sed risus.
+            </p>
         </div>
-       <div className='md:max-w-[1440px] flex-col lg:flex-row'>
-                    <div className='max-w-[300px] md:max-w-[550px] border-2 border-orange-500 rounded-2xl min-h-[500px] max-h-[500px]'>
-                        <h3 className='text-center text-lg font-semibold mt-[20px]'>Marketing Digital</h3>
-                        <p className='text-left text-base font-light'>
-                            Norem ipsum dolor sit amet, consectetur adipiscing elit. 
-                            Etiam eu turpis molestie, dictum est a, mattis tellus. 
-                            Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, 
-                            ut interdum tellus elit sed risus.
-                        </p>
-                    </div>
-                    <div className='max-w-[300px] md:max-w-[550px] border-2 border-orange-500 rounded-2xl min-h-[500px] max-h-[500px] mt-[50px]'>
-                        <h3 className='text-center text-lg font-semibold mt-[20px]'>Desarrollo Web</h3>
-                        <p className='text-left text-base font-light'>
-                            Norem ipsum dolor sit amet, consectetur adipiscing elit. 
-                            Etiam eu turpis molestie, dictum est a, mattis tellus. 
-                            Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, 
-                            ut interdum tellus elit sed risus.
-                        </p>
-                    </div>
-                    <div className='max-w-[300px] md:max-w-[550px] border-2 border-orange-500 rounded-2xl min-h-[500px] max-h-[500px] mt-[50px]'>
-                        <h3 className='text-center text-lg font-semibold mt-[20px]'>Paid Ads</h3>
-                        <p className='text-left text-base font-light'>
-                            Norem ipsum dolor sit amet, consectetur adipiscing elit. 
-                            Etiam eu turpis molestie, dictum est a, mattis tellus. 
-                            Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, 
-                            ut interdum tellus elit sed risus.
-                        </p>
-                    </div>
+
+        {/* Card 2 */}
+        <div className='max-w-[300px] md:max-w-[400px] border-2 border-orange-500 rounded-2xl min-h-[500px] max-h-[500px] mt-[50px] lg:mt-0'>
+            <Image className='max-h-[250px] rounded-t-[16px] object-cover' src={Test} alt="" />
+            <h3 className='text-left px-4 text-orange-500 text-2xl font-bold mt-[20px]'>Desarrollo Web</h3>
+            <p className='text-left text-base font-light px-4'>
+                Norem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Etiam eu turpis molestie, dictum est a, mattis tellus. 
+                Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, 
+                ut interdum tellus elit sed risus.
+            </p>
+        </div>
+
+        {/* Card 3 */}
+        <div className='max-w-[300px] md:max-w-[400px] border-2 border-orange-500 rounded-2xl min-h-[500px] max-h-[500px] mt-[50px] lg:mt-0'>
+            <Image className='max-h-[250px] rounded-t-[16px] object-cover' src={Test} alt="" />
+            <h3 className='text-left px-4 text-orange-500 text-2xl font-bold mt-[20px]'>Paid Ads</h3>
+            <p className='text-left text-base font-light px-4'>
+                Norem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Etiam eu turpis molestie, dictum est a, mattis tellus. 
+                Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, 
+                ut interdum tellus elit sed risus.
+            </p>
         </div>
     </div>
+</div>
   )
 }
 
